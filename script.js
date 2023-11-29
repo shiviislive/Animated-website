70115896b99646a5917324ea1f19a8a0f5c43b02
shiviislive <sa9598326@gmail.com>
2023-11-30 00:00:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
+gsap.from(".nlink", {
+    stagger: .2,
+    y: 10,
+    duration: 1,
+    ease:Power2,
+    opacity: 0
+})
+Shery.textAnimate("#headings h1", {
+    style: 2,
+    y: 10,
+    delay: 0.3,
+    duration: 2,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    multiplier:0.1,
+  });
+
+  gsap.from(".anim2", {
+    y: 50,
+    stagger:.3,
+    opacity: 0,
+    ease: Expo,
+    duration: 1
+
+  });
+
+ 
+
+document.querySelector("#ftext button")
+.addEventListener("mouseover", function(){
+    gsap.to("#future video",{
+        opacity: 1,
+        duration: 1,
+        ease: Power4
+    })
+})
+
+document.querySelector("#ftext button")
+.addEventListener("mouseleave", function(){
+    gsap.to("#future video",{
+        opacity: 0,
+        duration: 1,
+        ease: Power4
+    })
+})
